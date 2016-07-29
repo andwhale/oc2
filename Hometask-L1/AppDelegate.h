e3f9754a80dc0073ab12c748b75fde8be4319ee9
andwhale <andwhale@gmail.com>
2016-07-30 00:00:58 +0300

    Initial Commit

---
+++ Hometask-L1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hometask-L1
+//
+//  Created by Andrew Velikiy on 7/30/16.
+//  Copyright © 2016 Andrew Velikiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hometask-L1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hometask-L1
+//
+//  Created by Andrew Velikiy on 7/30/16.
+//  Copyright © 2016 Andrew Velikiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
